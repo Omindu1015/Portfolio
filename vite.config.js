c3e54be1896fd76662f2aa5eedaaf4ab1d1127bf
Omindu1015 <omindu.20230092@iit.ac.lk>
2025-05-31 23:33:59 +0530

    update files

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
